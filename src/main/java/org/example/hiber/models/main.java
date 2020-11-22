e0664be9a97e1d80d11bffa1d43788231eb8c21c
ladmin <kivelofff@gmail.com>
2020-11-22 22:50:49 +0100

    second commit

--- src/main/java/org/example/hiber/models/main.java
+++ src/main/java/org/example/hiber/models/main.java
@@ -7,7 +7,18 @@ public class main {
         userService.saveUser(user1);
         Auto car1 = new Auto("Camry", "black");
         car1.setUser(user1);
-        
+        user1.addAuto(car1);
+        Auto car2 = new Auto("Solaris", "yellow");
+        car2.setUser(user1);
+        user1.addAuto(car2);
+        User user2 = new User("Angella", 25);
+        userService.saveUser(user2);
+        Auto car3 = new Auto("Juke", "white");
+        car3.setUser(user2);
+        user2.addAuto(car3);
+        userService.updateUser(user1);
+        userService.updateUser(user2);
 
+        System.out.println(userService.findAllUsers().toString());
     }
 }
